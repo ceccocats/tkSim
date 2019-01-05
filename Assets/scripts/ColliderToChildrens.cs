a8b9962409b7a912d9b423dd53f631753c1a317b
Francesco Gatti <gattifrancesco@hotmail.it>
2019-01-05 22:10:51 +0100

    better ing model

---
+++ Assets/scripts/ColliderToChildrens.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderToChildrens : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// Iterate through all child objects of our Geometry object
+		foreach (Transform childObject in transform)
+		{
+			// First we get the Mesh attached to the child object
+			Mesh mesh = childObject.gameObject.GetComponent<MeshFilter>().mesh;
+
+			// If we've found a mesh we can use it to add a collider
+			if (mesh != null)
+			{                      
+				// Add a new MeshCollider to the child object
+				MeshCollider meshCollider = childObject.gameObject.AddComponent<MeshCollider>();
+
+				// Finaly we set the Mesh in the MeshCollider
+				meshCollider.sharedMesh = mesh;
+			}
+		}		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
