a48d27f42d38eeeeceffa8bb680c1c875281a288
Francesco Gatti <gattifrancesco@hotmail.it>
2020-04-05 20:17:19 +0200

    pointcloud2

--- Assets/Ros/RosSharp/scripts/PointCloudPublisher.cs
+++ Assets/Ros/RosSharp/scripts/PointCloudPublisher.cs
@@ -13,18 +13,20 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+using System;
 using UnityEngine;
 using RosSharp.RosBridgeClient.MessageTypes.Geometry;
 using RosSharp.RosBridgeClient.MessageTypes.Sensor;
 
 namespace RosSharp.RosBridgeClient
 {
-    public class PointCloudPublisher : UnityPublisher<MessageTypes.Sensor.PointCloud>
+    public class PointCloudPublisher : UnityPublisher<MessageTypes.Sensor.PointCloud2>
     {
         public VeloLidar lidar;
         public string FrameId = "Unity";
 
         private MessageTypes.Sensor.PointCloud message;
+        private MessageTypes.Sensor.PointCloud2 finalMsg;
         private float scanPeriod;
         private float previousScanTime = 0;
                 
@@ -52,6 +54,7 @@ namespace RosSharp.RosBridgeClient
             {
                 header = new MessageTypes.Std.Header { frame_id = FrameId }
             };
+            finalMsg = new MessageTypes.Sensor.PointCloud2();
         }
 
         private void UpdateMessage()
@@ -79,8 +82,58 @@ namespace RosSharp.RosBridgeClient
                 message.points[i].z =  lidar.points[i].y;
                 //message.channels[0].values[i] = 0;
             }
+
+            convertPointCloudToPointCloud2();
+            Publish(finalMsg);
+        }
+
+        private void convertPointCloudToPointCloud2() {
+            finalMsg.header = message.header;
+            finalMsg.width  = (uint) message.points.Length;
+            finalMsg.height = (uint) 1;
+            Array.Resize(ref finalMsg.fields, 3 + message.channels.Length);
+            for(int i=0; i<finalMsg.fields.Length; i++) 
+                finalMsg.fields[i] = new PointField();
             
-            Publish(message);
+            // Convert x/y/z to fields
+            finalMsg.fields[0].name = "x"; finalMsg.fields[1].name = "y"; finalMsg.fields[2].name = "z";
+            int offset = 0;
+            // All offsets are *4, as all field data types are float32
+            for (int d = 0; d < finalMsg.fields.Length; ++d, offset += 4)
+            {
+                finalMsg.fields[d].offset = (uint) offset;
+                finalMsg.fields[d].datatype = MessageTypes.Sensor.PointField.FLOAT32;
+                finalMsg.fields[d].count  = 1;
+            }
+            finalMsg.point_step = (uint) offset;
+            finalMsg.row_step   = finalMsg.point_step * finalMsg.width;
+            // Convert the remaining of the channels to fields
+            for (int d = 0; d < message.channels.Length; ++d)
+                finalMsg.fields[3 + d].name = message.channels[d].name;
+            
+            Array.Resize(ref finalMsg.data, (int) (message.points.Length * finalMsg.point_step));
+            finalMsg.is_bigendian = false;  // @todo ?
+            finalMsg.is_dense     = false;
+
+            // Copy the data points
+            for (int cp = 0; cp < message.points.Length; ++cp) {
+                byte[] x_bytes = BitConverter.GetBytes(message.points[cp].x); 
+                byte[] y_bytes = BitConverter.GetBytes(message.points[cp].y); 
+                byte[] z_bytes = BitConverter.GetBytes(message.points[cp].z); 
+                Array.Copy(x_bytes, 0, finalMsg.data, cp * finalMsg.point_step + finalMsg.fields[0].offset, sizeof(float));
+                Array.Copy(y_bytes, 0, finalMsg.data, cp * finalMsg.point_step + finalMsg.fields[1].offset, sizeof(float));
+                Array.Copy(z_bytes, 0, finalMsg.data, cp * finalMsg.point_step + finalMsg.fields[2].offset, sizeof(float));
+
+                for (int d = 0; d < message.channels.Length; ++d) {
+                    if (message.channels[d].values.Length == message.points.Length) {
+                        byte[] c_bytes = BitConverter.GetBytes(message.channels[d].values[cp]); 
+                        Array.Copy(c_bytes, 0, finalMsg.data, cp * finalMsg.point_step + finalMsg.fields[3 + d].offset, sizeof(float));
+                    }
+                }
+
+            }
+
+            return;
         }
     }
 }
