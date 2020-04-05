6884b2861e702b470e6cdde8d2e51e1ad7673b9a
Francesco Gatti <gattifrancesco@hotmail.it>
2020-04-05 19:28:54 +0200

    point cloud pub

---
+++ Assets/Ros/RosSharp/scripts/PointCloudPublisher.cs
@@ -0,0 +1,86 @@
+﻿/*
+© Siemens AG, 2018
+Author: Berkay Alp Cakal (berkay_alp.cakal.ct@siemens.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+<http://www.apache.org/licenses/LICENSE-2.0>.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using UnityEngine;
+using RosSharp.RosBridgeClient.MessageTypes.Geometry;
+using RosSharp.RosBridgeClient.MessageTypes.Sensor;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class PointCloudPublisher : UnityPublisher<MessageTypes.Sensor.PointCloud>
+    {
+        public VeloLidar lidar;
+        public string FrameId = "Unity";
+
+        private MessageTypes.Sensor.PointCloud message;
+        private float scanPeriod;
+        private float previousScanTime = 0;
+                
+
+        protected override void Start()
+        {
+            base.Start();
+            InitializeMessage();
+        }
+
+        private void FixedUpdate()
+        {
+            if (Time.realtimeSinceStartup >= previousScanTime + scanPeriod)
+            {
+                UpdateMessage();
+                previousScanTime = Time.realtimeSinceStartup;
+            }
+        }
+
+        private void InitializeMessage()
+        {
+            scanPeriod = lidar.period;
+
+            message = new MessageTypes.Sensor.PointCloud
+            {
+                header = new MessageTypes.Std.Header { frame_id = FrameId }
+            };
+        }
+
+        private void UpdateMessage()
+        {
+            message.header.Update();
+            
+            lidar.scan();
+            
+            // allocate if necessary
+            if(message.points.Length != lidar.points.Length) {
+                Debug.Log("Allocate points: " + lidar.points.Length);
+                message.points = new Point32[lidar.points.Length];
+                for(int i=0; i<lidar.points.Length; i++) {
+                    message.points[i] = new Point32(0,0,0);    
+                }
+                //message.channels = new ChannelFloat32[1];
+                //message.channels[0].name = "intensity";
+                //message.channels[0].values = new float[lidar.points.Length];
+            }
+            
+            for(int i=0; i<lidar.points.Length; i++) {
+                // unity2ros
+                message.points[i].x =  lidar.points[i].z;
+                message.points[i].y = -lidar.points[i].x;
+                message.points[i].z =  lidar.points[i].y;
+                //message.channels[0].values[i] = 0;
+            }
+            
+            Publish(message);
+        }
+    }
+}
---
+++ Assets/Ros/RosSharp/scripts/VeloLidar.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VeloLidar : MonoBehaviour {
+
+	public bool debugRay = false;
+    public float maxAngle = 10;
+    public float minAngle = -10;
+    public int numberOfLayers = 16;
+    public int numberOfIncrements = 360;
+    public float maxRange = 100f;
+    public float period = 0.1f;
+
+    float vertIncrement;
+    float azimutIncrAngle;
+
+    [HideInInspector]
+    public Vector3[] points;
+
+    // Use this for initialization
+    void Start () {
+        points = new Vector3[numberOfLayers* numberOfIncrements];
+        vertIncrement = (float)(maxAngle - minAngle) / (float)(numberOfLayers - 1);
+        azimutIncrAngle = (float)(360.0f / numberOfIncrements);
+    }
+
+
+	public void scan() {
+        Vector3 nopoint = new Vector3(0, 0, 0);
+        Vector3 fwd = new Vector3(0, 0, 1);
+        Vector3 dir;
+        RaycastHit hit;
+        int indx = 0;
+        float angle;
+
+        //azimut angles
+        for (int incr = 0; incr < numberOfIncrements; incr++)
+        {
+            for (int layer = 0; layer < numberOfLayers; layer++)
+            {
+                //print("incr "+ incr +" layer "+layer+"\n");
+                indx = layer + incr * numberOfLayers;
+                angle = minAngle + (float)layer * vertIncrement;
+                float azimut = incr * azimutIncrAngle;
+                dir = transform.rotation * Quaternion.Euler(-angle, azimut, 0)*fwd;
+
+                if (Physics.Raycast(transform.position, dir, out hit, maxRange))
+                {
+                    points[indx] = transform.InverseTransformPoint(hit.point);
+                }
+                else
+                {
+                    points[indx] = nopoint;
+                }
+				if(debugRay)
+					Debug.DrawRay(transform.position, dir * hit.distance, Color.green);
+
+            }
+        }
+
+    }
+}
